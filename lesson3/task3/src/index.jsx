be161bcb7a8354fd3c04e3c3703b4bdd725a0d5d
JeePik <andrewlegeza@gmail.com>
2022-05-23 19:10:20 +0300

    added files lesson3 task3

---
+++ lesson3/task3/src/index.jsx
@@ -0,0 +1,9 @@
+import "./styles.scss";
+import ReactDOM from "react-dom";
+import Comment from "./Comment.jsx";
+import React from "react";
+import App from "./App.jsx";
+
+const rootElem = document.querySelector("#root");
+
+ReactDOM.render(<App />, rootElem);
