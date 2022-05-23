e519cd93c18b82713903e02f08dab03122edee36
JeePik <andrewlegeza@gmail.com>
2022-05-23 16:25:02 +0300

    added files lesson3 task1

---
+++ lesson3/task1/src/index.jsx
@@ -0,0 +1,9 @@
+import "./index.scss";
+import "./search.scss";
+import React from "react";
+import ReactDOM from "react-dom";
+import Search from "./Search.jsx";
+
+const rootElem = document.querySelector("#root");
+
+ReactDOM.render(<Search name="Tom" age={17} />, rootElem);
