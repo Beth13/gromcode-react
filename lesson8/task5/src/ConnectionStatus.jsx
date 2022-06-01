ec246a2d80f0d307f4ef415e40c88029916a9473
JeePik <andrewlegeza@gmail.com>
2022-06-01 17:58:44 +0300

    added files lesson8 task5

---
+++ lesson8/task5/src/ConnectionStatus.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+class ConnectionStatus extends Component {
+  state = {
+    isOnline: true,
+  };
+
+  componentDidMount() {
+    window.addEventListener("offline", this.setOfflineStatus);
+    window.addEventListener("online", this.setOnlineStatus);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("offline", this.setOfflineStatus);
+    window.removeEventListener("online", this.setOnlineStatus);
+  }
+
+  setOnlineStatus = () => {
+    this.setState({
+      isOnline: true,
+    });
+  };
+
+  setOfflineStatus = () => {
+    this.setState({
+      isOnline: false,
+    });
+  };
+
+  render() {
+    return !this.state.isOnline ? (
+      <div className="status status_offline">Offline</div>
+    ) : (
+      <div className="status">Online</div>
+    );
+  }
+}
+
+export default ConnectionStatus;
