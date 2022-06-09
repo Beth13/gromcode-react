105ee238044d5a59f6f05ad0cebcd73f2bc33ea6
JeePik <andrewlegeza@gmail.com>
2022-06-09 20:18:53 +0300

    added files lesson13 task2

--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 
-function Product() {
-  const { productId } = useParams();
-  return <div className="page__content">{`Product is a ${productId}`}</div>;
+function Product({ match }) {
+  return (
+    <div className="page__content">{`Product is a ${match.params.productId}`}</div>
+  );
 }
 
 export default Product;
