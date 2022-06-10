24a16d86d99c2c737f79676f7d580fe74dfce0a1
JeePik <andrewlegeza@gmail.com>
2022-06-10 19:05:51 +0300

    added files lesson13 task2

--- lesson13/task2/src/Product.jsx
+++ lesson13/task2/src/Product.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 function Product({ match }) {
-  return (
-    <div className="page__content">{`Product is a ${match.params.productId}`}</div>
-  );
+  return <div className="page__content">{match.params.productId}</div>;
 }
 
 export default Product;
